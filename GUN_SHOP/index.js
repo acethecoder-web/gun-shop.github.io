3340968d510c5f642b0f1b6727104f4a8dd160be
eysssss <102285503+acethecoder-web@users.noreply.github.com>
2024-02-24 06:45:08 -0800

    Add files via upload

---
+++ GUN_SHOP/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+  });
+
+
+
+        
